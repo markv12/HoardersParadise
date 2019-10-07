a52a501e3b20a27369ad00b46245c633e78256ba
Scott <swsteffes@gmail.com>
2019-10-08 02:34:13 +0900

    Change how rats work

--- Assets/Scripts/FilthSpawner.cs
+++ Assets/Scripts/FilthSpawner.cs
@@ -7,19 +7,40 @@ public class FilthSpawner : MonoBehaviour
     public MoveableItem ratPrefab;
     public Transform filthCreatureSpawnPosition;
 
-    private static readonly WaitForSeconds theWait = new WaitForSeconds(25);
+    public GameObject[] theRats;
+
+    private static readonly WaitForSeconds theWait = new WaitForSeconds(1.5f);
     IEnumerator Start()
     {
+        theRats = new GameObject[5];
+        for (int i = 0; i < theRats.Length; i++) {
+            GameObject newRat = SpawnFilthCreature();
+            newRat.SetActive(false);
+            theRats[i] = newRat;
+        }
         while (true) {
             yield return theWait;
             float filth = statUIManager.Filth;
-            if(UnityEngine.Random.Range(0.0f, 100.0f) < filth) {
-                SpawnFilthCreature();
+            int ratCount = Mathf.FloorToInt(filth / 20f);
+            for (int i = 0; i < ratCount; i++) {
+                GameObject rat = theRats[i];
+                if (!rat.activeSelf) {
+                    rat.SetActive(true);
+                }
+            }
+            for (int i = ratCount; i < theRats.Length; i++) {
+                GameObject rat = theRats[i];
+                if (rat.activeSelf) {
+                    rat.SetActive(false);
+                }
             }
         }
     }
 
-    private void SpawnFilthCreature() {
-        ComputerUIManager.instance.InstantiateItem(ratPrefab, filthCreatureSpawnPosition.position);
+    private GameObject SpawnFilthCreature() {
+        GameObject newRat = Instantiate(ratPrefab.gameObject);
+        newRat.transform.position = filthCreatureSpawnPosition.position;
+        StatUIManager.instance.RegisterItem(newRat.GetComponent<MoveableItem>());
+        return newRat;
     }
 }
